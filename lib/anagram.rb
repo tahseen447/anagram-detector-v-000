244f23b6b00bcd70eda5862fcab4ae5ce258bebc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-09 20:56:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+  attr_accessor :keyword
+
+  def initialize(keyword)
+    @keyword = keyword
+  end
+
+  def match(sentence)
+    key_array = keyword.split("").sort
+    #puts "#{sentence}, #{keyword}, #{key_array.inspect}"
+    result = []
+    sort_word_array = sentence.map { |word| word.split("").sort }
+    sort_word_array.each_with_index {|word, index| if word == key_array then result << sentence[index] end}
+    result
+  end
+end
