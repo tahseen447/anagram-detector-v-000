be4d760e236abc1e9c3f09694ba98255113d8c06
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-09 21:00:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(sentence)
     key_array = keyword.split("").sort
-    #puts "#{sentence}, #{keyword}, #{key_array.inspect}"
+    #sentence.select {|word| word.split("").sort == @keyword.split("").sort}
     result = []
     sort_word_array = sentence.map { |word| word.split("").sort }
     sort_word_array.each_with_index {|word, index| if word == key_array then result << sentence[index] end}
